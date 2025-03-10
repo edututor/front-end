d2c97d9e5eeef8a8c4b7b72ab33f6785a83d75f8
Aytekin97 <aytecha.97@gmail.com>
2025-03-10 11:04:22 -0400

    Import useCallback

--- src/components/QuizzesComponent.js
+++ src/components/QuizzesComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import '../styles/Quizzes.css';
 
 const FETCH_QUIZZES_URL = process.env.REACT_APP_FETCH_QUIZZES_URL;
